d970ad9994e1cdbc94143bc349a9f60d4ab2e26b
何帅辰 <44659719+heshuaichen@users.noreply.github.com>
2020-10-01 20:25:32 +0800

    Add files via upload

---
+++ build/precache-manifest.2056019e1d92bd52191318ad0b0ab334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95831291c39383c376d0db1893729d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3251dd3f6445c84c4cfd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e0b2b70f6abe4a16f86f",
+    "url": "/static/js/2.2f9797ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f9797ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3251dd3f6445c84c4cfd",
+    "url": "/static/js/main.a2205fe6.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
